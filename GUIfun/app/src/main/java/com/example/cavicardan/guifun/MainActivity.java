00bf8d5321f0a0fbad76b82f3288aecb233b5055
CaviCardan <marcinjaj@gmail.com>
2016-12-05 23:20:56 +0100

    Menus

--- GUIfun/app/src/main/java/com/example/cavicardan/guifun/MainActivity.java
+++ GUIfun/app/src/main/java/com/example/cavicardan/guifun/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.cavicardan.guifun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
 import android.text.InputType;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,6 +17,7 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static android.text.InputType.TYPE_CLASS_TEXT;
 
@@ -28,7 +32,8 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private Switch switcher;
     private Spinner spinner;
-
+    private Button showpop;
+    private Button contexM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,27 +44,72 @@ public class MainActivity extends AppCompatActivity {
         editText = (EditText) findViewById(R.id.editText);
         textLabel2= (TextView) findViewById(R.id.textView);
         button = (Button) findViewById(R.id.button);
+        showpop = (Button) findViewById(R.id.show_pop);
         radioG= (RadioGroup) findViewById(R.id.radioGr1);
         switcher=(Switch) findViewById(R.id.switch1);
         passText=(EditText) findViewById(R.id.PassText);
+        contexM= (Button) findViewById(R.id.contex_menu);
         //Dokładamy rzeczy do spinnera
        spinner=(Spinner) findViewById(R.id.spinner);
 
-        ArrayAdapter<CharSequence> adapter =ArrayAdapter.createFromResource(this,R.array.country_arrays, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter =ArrayAdapter.
+                createFromResource(this,R.array.country_arrays, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
-
-
         //robimy coś z tym że wybraliśmy coś z radio buttonów
         addListenerRadioButtons();
-
         addListenerToButton();
-
         addListenerToSwitch();
-
         addListenerToSpinner();
 
+        addPopUpMenu();
+
+        registerForContextMenu(contexM);
+    }
+
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Select The Action");
+        menu.add(0, v.getId(), 0, "Call");//groupId, itemId, order, title
+        menu.add(0, v.getId(), 0, "SMS");
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        if(item.getTitle()=="Opcja 1"){
+            Toast.makeText(getApplicationContext(),"Wybrano opcję 1",Toast.LENGTH_LONG).show();
+        }
+        else if(item.getTitle()=="Opcja 2"){
+            Toast.makeText(getApplicationContext(),"Wybrano opcję 2",Toast.LENGTH_LONG).show();
+        }else{
+            return false;
+        }
+        return true;
+    }
+
+
+    private void addPopUpMenu() {
+        showpop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Creating the instance of PopupMenu
+                PopupMenu popup = new PopupMenu(MainActivity.this, showpop);
+                //Inflating the Popup using xml file
+                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+                //registering popup with OnMenuItemClickListener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(MainActivity.this,"You Clicked : " + item.getTitle(),Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                popup.show();//showing popup menu
+            }
+        });
     }
 
     private void addListenerToSpinner() {
